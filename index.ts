fad05b44873e88eca2b20e19e95f9bf1feb56903
iTNTPiston <zhaoyizhu2000@gmail.com>
2022-02-27 13:18:49 -0800

    using export type on index.ts

--- index.ts
+++ index.ts
@@ -1,11 +1,14 @@
 // Expose types and functions to engine side
-export {
+export type {
     RouteScript,
     RouteMetadata,
     RouteSection,
     RouteModule,
     RouteStep,
     RouteScriptExtend,
+} from "./src/ts/RouteScript";
+export {
+    TARGET_VERSION,
     switchSection,
     switchModule,
     switchStep
